96679242eb28a48e5cf17c163814eb5a71536934
Nick Dainty <nick@npad.co.uk>
2012-05-17 21:36:46 +0100

    Homework 3: HTML Tags

---
+++ homework_3/html_tags.py
@@ -0,0 +1,49 @@
+# ---------------
+# User Instructions
+#
+# Write a function, findtags(text), that takes a string of text
+# as input and returns a list of all the html start tags in the 
+# text. It may be helpful to use regular expressions to solve
+# this problem.
+
+import re
+
+ws = r"\s*"
+key = r"\w+"
+value = r"\"[^\"]*\""
+tag = "<{ws}{key}{ws}(?:{key}{ws}={ws}{value})*{ws}>".format(ws=ws, key=key, value=value)
+pattern = re.compile(tag)
+
+def findtags(text):
+    return pattern.findall(text)
+
+testtext1 = """
+My favorite website in the world is probably 
+<a href="www.udacity.com">Udacity</a>. If you want 
+that link to open in a <b>new tab</b> by default, you should
+write <a href="www.udacity.com"target="_blank">Udacity</a>
+instead!
+"""
+
+testtext2 = """
+Okay, so you passed the first test case. <let's see> how you 
+handle this one. Did you know that 2 < 3 should return True? 
+So should 3 > 2. But 2 > 3 is always False.
+"""
+
+testtext3 = """
+It's not common, but we can put a LOT of whitespace into 
+our HTML tags. For example, we can make something bold by
+doing <         b           > this <   /b    >, Though I 
+don't know why you would ever want to.
+"""
+
+def test():
+    assert findtags(testtext1) == ['<a href="www.udacity.com">',
+                                   '<b>',
+                                   '<a href="www.udacity.com"target="_blank">']
+    assert findtags(testtext2) == []
+    assert findtags(testtext3) == ['<         b           >']
+    return 'tests pass'
+
+print test()
